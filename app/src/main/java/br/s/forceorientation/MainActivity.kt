b042675bb9d792b93687ef7d7293ecab5944e1a9
Lucas <lucas.nepomuceno1999@gmail.com>
2019-11-05 19:23:02 -0300

    adding layout of main activity

--- app/src/main/java/br/s/forceorientation/MainActivity.kt
+++ app/src/main/java/br/s/forceorientation/MainActivity.kt
@@ -3,12 +3,19 @@ package br.s.forceorientation
 import android.content.pm.ActivityInfo
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        text.setText("Screen normal")
+    }
+
+    override fun onRestoreInstanceState(savedInstanceState: Bundle) {
+        super.onRestoreInstanceState(savedInstanceState)
 
     }
 }
